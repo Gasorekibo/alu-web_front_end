a827e6c3345b57d8c781aac38a8145de3de35744
Gasorekibo <m.gasore@alustudent.com>
2024-03-15 10:32:27 +0200

    total execution time for many function call

--- Javascript_advanced/10-prime.js
+++ Javascript_advanced/10-prime.js
@@ -1,145 +1,36 @@
-function countPrimeNumbers() {
-  for (let i = 2; i <= 100; i++) {
-    let isPrime = i % 2 !== 0;
-    if (isPrime) {
-      console.log(
-        `Execution time of calculating prime numbers 100 times was ${performance.now()} milliseconds.`
-      );
+function isPrime(number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
     }
   }
+  return true;
 }
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
-countPrimeNumbers();
+
+function countPrimeNumbers(start, end) {
+  const startTime = performance.now();
+
+  let count = 0;
+  for (let i = start; i <= end; i++) {
+    if (isPrime(i)) {
+      count++;
+    }
+  }
+
+  const endTime = performance.now();
+  const executionTime = endTime - startTime;
+
+  return executionTime;
+}
+let totalExecutionTime = 0;
+let callTime = 1;
+while (callTime <= 100) {
+  totalExecutionTime += countPrimeNumbers(2, 100);
+  callTime++;
+}
+console.log(
+  `Execution time of calculating prime numbers 100 times was ${totalExecutionTime} milliseconds.`
+);
