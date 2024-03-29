a9d08f85de9e487dec00f0e1ba740796c761560d
Gasorekibo <m.gasore@alustudent.com>
2024-03-29 13:01:46 +0200

    update code to meet requirementa

--- Cookies_local_storage/src/index.js
+++ Cookies_local_storage/src/index.js
@@ -1,13 +1,13 @@
 const firstName = document.getElementById("name");
 const email = document.getElementById( "email" );
 document.getElementById("setCookies").addEventListener('click',  function setCookies(){
-    document.cookie = `firstName= ${firstName.value}`;
-    document.cookie = `email=${email.value};`;
+    document.cookie = `firstName= ${firstName.value}, email=${email.value}`;
 });
 
 document.getElementById("getCookies").addEventListener('click', function getCookie(){
-    const cookieValue = document.cookie.split(";");
-    const p = document.createElement('p').innerHTML = `firstName: ${cookieValue[0].split("=")[1]} , email: ${cookieValue[1].split("=")[1]}`;
+    console.log(document.cookie)
+    const cookieValue = document.cookie.split(",");
+    const p = document.createElement('p').innerHTML = `Cookies: ${cookieValue[0]}; ${cookieValue[1]}`;
     document.body.append(p)
 
 })
